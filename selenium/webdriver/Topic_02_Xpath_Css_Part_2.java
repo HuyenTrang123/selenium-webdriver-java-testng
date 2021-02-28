ba29fc741bd06793372fa1152db32beb18b860fe
Huyen Trang <37400622+HuyenTrang123@users.noreply.github.com>
2021-03-01 06:37:18 +0700

    TOPIC 6

--- selenium/webdriver/Topic_02_Xpath_Css_Part_2.java
+++ selenium/webdriver/Topic_02_Xpath_Css_Part_2.java
@@ -1,39 +1,35 @@
 package webdriver;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_01_Template {
+public class Topic_02_Xpath_Css_Part_2 {
 	WebDriver driver;
-//Test push code
+
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
-
-	@Test
-	public void TC_01_() {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
 
 	@Test
-	public void TC_02_() {
-	
+	public void TC_01_Xpath() {
+		driver.get("http://live.demoguru99.com/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
 	}
 
-	@Test
-	public void TC_03_() {
-		
-	}
 
 	@AfterClass
 	public void afterClass() {
-		//driver.quit();
+	driver.quit();
 	}
 
 }
